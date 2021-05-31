2a2ccbb06d43b254256791e0d2d496284b9fc358
Dave Defourneaux <dave.defourneaux@gmail.com>
2021-05-31 16:00:06 -0400

    Add May 12th Challenge

---
+++ scripts/May12thChallenge.js
@@ -0,0 +1,13 @@
+// Implement a function that adds two numbers together and returns the sum in binary.
+// The conversion can occur either before or after the calculation.
+
+function addNumbers(num1, num2){
+    let result = num1 + num2;
+    return result.toString(2);
+}
+
+document.getElementById('result-box').innerHTML = 
+`512 + 200 = 712 => in binary: ${addNumbers(512,200)}<br>
+17 + 19 = 36 => in binary: ${addNumbers(17,19)}<br>
+3000 + 487 = 3487 => in binary: ${addNumbers(3000,487)}`;
+
