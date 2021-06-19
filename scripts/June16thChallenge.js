910733d4ba318765a583a100ddf89518e71cd913
Dave Defourneaux <dave.defourneaux@gmail.com>
2021-06-19 15:06:05 -0400

    Solve Challenge

--- scripts/June16thChallenge.js
+++ scripts/June16thChallenge.js
@@ -20,11 +20,46 @@ examples = `There is 1 A table and 2 B tables. The array represents the groups c
     restaurant(1, 1, [1, 1, 2, 1])  =>  2`;
 
 function seatPatrons(A_tables, B_tables, patronGroups) {
-    console.log(A_tables, B_tables, patronGroups);
+
+    let denied = 0;
+
+    for (let i=0; i<patronGroups.length; i++) {
+
+        if (patronGroups[i] == 1 && A_tables > 0) {        
+            A_tables --;
+        }
+        else if (patronGroups[i] == 1 && A_tables == 0 && B_tables > 0) {
+            B_tables --;
+            A_tables ++;
+        }
+        else if (patronGroups[i] == 1 && A_tables == 0 && B_tables == 0) {
+            denied ++;
+        }
+        else if (patronGroups[i] == 2 && B_tables > 0) {
+            B_tables --;
+        }
+        else {
+            denied +=2;
+        }
+
+    }
+    return denied;        
 }
 
 // Output
 document.getElementById('instructions-box').innerHTML = rules;
 document.getElementById('examples-box').innerHTML = examples;
-
-document.getElementById('result-box').innerHTML = seatPatrons(1,2, [1,2,1,1]);
+document.getElementById('result-box').innerHTML = 
+`Number of 1-seat tables: 5<br>
+Number of 2-seat tables: 3<br>
+Order of patron groups: [1,2,2,1,1,1,2,2,1]<br>
+Number of patrons denied: ${seatPatrons(5,3, [1,2,2,1,1,1,2,2,1])}<br><br>
+Number of 1-seat tables: 4<br>
+Number of 2-seat tables: 2<br>
+Order of patron groups: [1,2,2,1,1,1,2,2,1,2]<br>
+Number of patrons denied: ${seatPatrons(4,2, [1,2,2,1,1,1,2,2,1,2])}<br><br>
+Number of 1-seat tables: 2<br>
+Number of 2-seat tables: 2<br>
+Order of patron groups: [1,2,2,1]<br>
+Number of patrons denied: ${seatPatrons(2,2, [1,2,2,1])}
+`
